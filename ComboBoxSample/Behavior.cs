bad38d501f814d3a5a46891458b73ae0a74dc2cc
Surya <surya.elayaperumal@syncfusion.com>
2024-01-11 15:20:40 +0530

    Commit the sample

---
+++ ComboBoxSample/Behavior.cs
@@ -0,0 +1,48 @@
+﻿using Syncfusion.Maui.Inputs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComboBoxSample
+{
+    public class SelectionChangedBehavior : Behavior<SfComboBox>
+    {
+        public Command Display { get; private set; }
+        EmployeeViewModel employeeViewModel;
+        protected override void OnAttachedTo(SfComboBox bindable)
+        {
+            bindable.SelectionChanged += Bindable_SelectionChanged;
+            base.OnAttachedTo(bindable);
+        }
+        protected override void OnDetachingFrom(SfComboBox bindable)
+        {
+            bindable.SelectionChanged -= Bindable_SelectionChanged;
+            base.OnDetachingFrom(bindable);
+        }
+
+        async void Bindable_SelectionChanged(object sender, Syncfusion.Maui.Inputs.SelectionChangedEventArgs e)
+        {
+            employeeViewModel = new EmployeeViewModel();
+            var selectedValue = e.CurrentSelection;
+            var lastItem = selectedValue.Last();
+            employeeViewModel.SelectedId = new List<string>();
+            foreach (var item in selectedValue)
+            {
+                var selectedId = (item as Employee).Id;
+                if (item == lastItem)
+                {
+                    employeeViewModel.SelectedId.Add(selectedId);
+                    Application.Current.MainPage.DisplayAlert("Selected Id", "The last selected ID is " + selectedId, "Ok");
+                }
+                else
+                {
+                    employeeViewModel.SelectedId.Add(selectedId);
+                }
+
+            }
+        }
+    }
+
+}
---
+++ ComboBoxSample/ViewModel.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using Syncfusion.Maui.Inputs;
+using System.Linq;
+
+namespace ComboBoxSample
+{
+    public class EmployeeViewModel : INotifyPropertyChanged
+    {
+
+        private ObservableCollection<object> tempselectedvalueCollection = new ObservableCollection<object>();
+
+        private ObservableCollection<Employee> employeeCollection;
+        public ObservableCollection<Employee> EmployeeCollection
+        {
+            get { return employeeCollection; }
+            set
+            {
+                employeeCollection = value;
+                RaisePropertyChanged("EmployeeCollection");
+
+            }
+        }
+
+        private ObservableCollection<string> selectionobject = new ObservableCollection<string>();
+        public ObservableCollection<string> SelectionObject
+        {
+            get { return selectionobject; }
+            set
+            {
+                selectionobject = value;
+                RaisePropertyChanged("SelectionObject");
+            }
+        }
+
+        private List<string> selectedId = new List<string>();
+        public List<string> SelectedId
+        {
+            get
+            {
+                return selectedId;
+            }
+            set
+            {
+                selectedId = value;
+                RaisePropertyChanged("SelectedId");
+            }
+        }
+
+        /// <summary>
+        /// MultiSelect Mode SelectedItem 
+        /// </summary>
+        private ObservableCollection<object> selectedvalueCollection = new ObservableCollection<object>();
+
+        public ObservableCollection<object> SelectedvalueCollection
+        {
+            get { return selectedvalueCollection; }
+            set
+            {
+                selectedvalueCollection = value;
+                RaisePropertyChanged("SelctedvalueCollection");
+
+            }
+        }
+
+
+        /// <summary>
+        /// MultiSelect Mode SelectedItem 
+        /// </summary>
+        private ObservableCollection<object> multiSelectedItem;
+
+        public ObservableCollection<object> MultiSelectedItem
+        {
+            get { return multiSelectedItem; }
+            set
+            {
+                multiSelectedItem = value;
+                RaisePropertyChanged("SelectedItems");
+
+                tempselectedvalueCollection.Clear();
+                foreach (var item in multiSelectedItem)
+                {
+                    tempselectedvalueCollection.Add((item as Employee).Id);
+                }
+                SelectedvalueCollection = tempselectedvalueCollection;
+            }
+        }
+
+        public EmployeeViewModel()
+        {
+            MultiSelectedItem = new ObservableCollection<object>();
+            employeeCollection = new ObservableCollection<Employee>();
+            employeeCollection.Add(new Employee() { Id = "1", Name = "John" });
+            employeeCollection.Add(new Employee() { Id = "2", Name = "James" });
+            employeeCollection.Add(new Employee() { Id = "3", Name = "Jacob" });
+            employeeCollection.Add(new Employee() { Id = "4", Name = "Joy" });
+            employeeCollection.Add(new Employee() { Id = "5", Name = "Justin" });
+            employeeCollection.Add(new Employee() { Id = "6", Name = "Jerome" });
+            employeeCollection.Add(new Employee() { Id = "7", Name = "Jessica" });
+            employeeCollection.Add(new Employee() { Id = "8", Name = "Victoria" });
+          
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(String name)
+        {
+            if (PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+
+   
+
+}
